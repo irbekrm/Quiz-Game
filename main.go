920c3494a95b64199d81854bb07f9fbfb49c88db
irbekrm <irbekrm@gmail.com>
2018-07-15 12:23:18 +0100

    User can now pass a filename via the f flag

--- main.go
+++ main.go
@@ -11,25 +11,31 @@ import (
 )
 
 
+var fn = "problems.csv"
 var correct int
 var incorrect int
 
 func helpPrinter() {
   text := "NAME\n\tQuizz Game\n\nDESCRIPTION\n\tAsks a number of quizz questions. " +
   "Waits for user's answer after each question. Prints the score at the end.\n\n" +
-  "-h\n\tprints a short description of the app and then exits\n\n"
+  "-h\n\tprints a short description of the app and then exits\n\n" +
+  "-f\n\taccepts name of a file to read quizzes from. It should be a csv file " +
+  "in format 'question, answer'. If no filename is passed, reads from the default quizzes file.\n\n"
   fmt.Println(text)
   os.Exit(0)
 }
 
 func main() {
+  n := flag.String("f", "", "provide file")
   h := flag.Bool("h", false, "print usage description")
   flag.Parse()
   if *h  {
     helpPrinter()
   }
+  if *n != "" {
+    fn = *n
+  }
   reader := bufio.NewReader(os.Stdin)
-  fn := "problems.csv"
 
   f, err := os.Open(fn)
   if err != nil {
