72fdd2538a3553d6e928aeda2ed59b1b3f5ac2a1
irbekrm <irbekrm@gmail.com>
2018-07-15 13:26:54 +0100

    formatting errors fixed

--- main.go
+++ main.go
@@ -1,83 +1,82 @@
 package main
 
 import (
-  "time"
-  "math/rand"
-  "flag"
-  "bufio"
-  "strings"
-  "os"
-  "fmt"
-  "encoding/csv"
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
-
 
 var fn = "problems.csv"
 var correct int
 var totalNumberOfQuestions int
 
 func shuffle(s [][]string) [][]string {
-  source := rand.NewSource(time.Now().UnixNano())
-  r := rand.New(source)
-  for i := range s {
-    newPosition := r.Intn(len(s) - 1)
-    s[i], s[newPosition] = s[newPosition], s[i]
-  }
-  return s
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range s {
+		newPosition := r.Intn(len(s) - 1)
+		s[i], s[newPosition] = s[newPosition], s[i]
+	}
+	return s
 }
 
-
 func helpPrinter() {
-  text := "NAME\n\tQuizz Game\n\nDESCRIPTION\n\tAsks a number of quizz questions. " +
-  "Waits for user's answer after each question. Prints the score at the end.\n\n" +
-  "-h\n\tprints a short usage description and then exits\n\n" +
-  "-f\n\taccepts name of a file to read quizzes from. It should be a csv file " +
-  "in format 'question, answer'. If no filename is passed, reads from the default quizzes file.\n\n" +
-  "-s\n\tshuffle the questions\n\n"
-  fmt.Println(text)
-  os.Exit(0)
+	text := "NAME\n\tQuizz Game\n\nDESCRIPTION\n\tAsks a number of quizz questions. " +
+		"Waits for user's answer after each question. Prints the score at the end.\n\n" +
+		"-h\n\tprints a short usage description and then exits\n\n" +
+		"-f\n\taccepts name of a file to read quizzes from. It should be a csv file " +
+		"in format 'question, answer'. If no filename is passed, reads from the default quizzes file.\n\n" +
+		"-s\n\tshuffle the questions\n\n"
+	fmt.Println(text)
+	os.Exit(0)
 }
 
 func main() {
-  n := flag.String("f", "", "provide filename")
-  h := flag.Bool("h", false, "print usage description")
-  s := flag.Bool("s", false, "shuffle the questions")
-  flag.Parse()
-  if *h  {
-    helpPrinter()
-  }
-  if *n != "" {
-    fn = *n
-  }
-  reader := bufio.NewReader(os.Stdin)
+	n := flag.String("f", "", "provide filename")
+	h := flag.Bool("h", false, "print usage description")
+	s := flag.Bool("s", false, "shuffle the questions")
+	flag.Parse()
 
-  f, err := os.Open(fn)
-  if err != nil {
-    fmt.Println("Error ", err)
-    os.Exit(1)
-  }
+	if *h {
+		helpPrinter()
+	}
+	if *n != "" {
+		fn = *n
+	}
+	reader := bufio.NewReader(os.Stdin)
 
-  r := csv.NewReader(f)
+	f, err := os.Open(fn)
+	if err != nil {
+		fmt.Println("Error ", err)
+		os.Exit(1)
+	}
 
-  records, err := r.ReadAll()
+	r := csv.NewReader(f)
 
-  if err != nil {
-    fmt.Println("Error", err)
-  }
+	records, err := r.ReadAll()
 
-  totalNumberOfQuestions = len(records)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
 
-  if *s {
-    records = shuffle(records)
-  }
+	totalNumberOfQuestions = len(records)
 
-  for _, record := range records {
-    fmt.Println("What is " + record[0] + "?")
-    input, _ := reader.ReadString('\n')
-    input = strings.TrimRight(input, "\n")
-    if input == record[1] {
-      correct++
-    }
-  }
-  fmt.Printf("Score: %d/%d\n", correct, totalNumberOfQuestions)
+	if *s {
+		records = shuffle(records)
+	}
+
+	for _, record := range records {
+		fmt.Println("What is " + record[0] + "?")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimRight(input, "\n")
+		if input == record[1] {
+			correct++
+		}
+	}
+	fmt.Printf("Score: %d/%d\n", correct, totalNumberOfQuestions)
 }
