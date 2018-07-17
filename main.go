0e11bca1b7048a6a560c6515fe20003a5f9b571a
irbekrm <irbekrm@gmail.com>
2018-07-17 11:17:01 +0100

    quizz now has a default timeout of 30s which can be increased by passing arg to -t flag

--- main.go
+++ main.go
@@ -25,13 +25,21 @@ func shuffle(s [][]string) [][]string {
 	return s
 }
 
+func sleeper(n int){
+  seconds := time.Duration(n) * 30 * time.Second
+  time.Sleep(seconds)
+  fmt.Println("Timeout!")
+  os.Exit(0)
+}
+
 func helpPrinter() {
 	text := "NAME\n\tQuizz Game\n\nDESCRIPTION\n\tAsks a number of quizz questions. " +
 		"Waits for user's answer after each question. Prints the score at the end.\n\n" +
 		"-h\n\tprints a short usage description and then exits\n\n" +
 		"-f\n\taccepts name of a file to read quizzes from. It should be a csv file " +
 		"in format 'question, answer'. If no filename is passed, reads from the default quizzes file.\n\n" +
-		"-s\n\tshuffle the questions\n\n"
+		"-s\n\tshuffle the questions\n\n" +
+    "-t\n\tprovide an int argument X to increase the default timeout value (30s) to X * 30s.\n\n"
 	fmt.Println(text)
 	os.Exit(0)
 }
@@ -40,6 +48,7 @@ func main() {
 	n := flag.String("f", "", "provide filename")
 	h := flag.Bool("h", false, "print usage description")
 	s := flag.Bool("s", false, "shuffle the questions")
+  t := flag.Int("t", 1, "timeout. requires an int argument")
 	flag.Parse()
 
 	if *h {
@@ -69,6 +78,8 @@ func main() {
 	if *s {
 		records = shuffle(records)
 	}
+
+  go sleeper(*t)
 
 	for _, record := range records {
 		fmt.Println("What is " + record[0] + "?")
