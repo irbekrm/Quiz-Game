b6f73ee689366debd58c86cd11d4608b58d8c15e
irbekrm <irbekrm@gmail.com>
2018-07-18 09:02:39 +0100

    after a timeout the correct score will still be printed out

--- main.go
+++ main.go
@@ -25,11 +25,10 @@ func shuffle(s [][]string) [][]string {
 	return s
 }
 
-func sleeper(n int){
-  seconds := time.Duration(n) * 30 * time.Second
-  time.Sleep(seconds)
-  fmt.Println("Timeout!")
-  os.Exit(0)
+func sleeper(n int, c chan bool) {
+	seconds := time.Duration(n) * 30 * time.Second
+	time.Sleep(seconds)
+	c <- true
 }
 
 func helpPrinter() {
@@ -39,16 +38,29 @@ func helpPrinter() {
 		"-f\n\taccepts name of a file to read quizzes from. It should be a csv file " +
 		"in format 'question, answer'. If no filename is passed, reads from the default quizzes file.\n\n" +
 		"-s\n\tshuffle the questions\n\n" +
-    "-t\n\tprovide an int argument X to increase the default timeout value (30s) to X * 30s.\n\n"
+		"-t\n\tprovide an int argument X to increase the default timeout value (30s) to X * 30s.\n\n"
 	fmt.Println(text)
 	os.Exit(0)
+}
+
+func asker(input chan string) {
+	for {
+		in := bufio.NewReader(os.Stdin)
+		result, err := in.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error", err)
+			os.Exit(1)
+		}
+		result = strings.TrimRight(result, "\n")
+		input <- result
+	}
 }
 
 func main() {
 	n := flag.String("f", "", "provide filename")
 	h := flag.Bool("h", false, "print usage description")
 	s := flag.Bool("s", false, "shuffle the questions")
-  t := flag.Int("t", 1, "timeout. requires an int argument")
+	t := flag.Int("t", 1, "timeout. requires an int argument")
 	flag.Parse()
 
 	if *h {
@@ -57,7 +69,6 @@ func main() {
 	if *n != "" {
 		fn = *n
 	}
-	reader := bufio.NewReader(os.Stdin)
 
 	f, err := os.Open(fn)
 	if err != nil {
@@ -79,15 +90,28 @@ func main() {
 		records = shuffle(records)
 	}
 
-  go sleeper(*t)
+	input := make(chan string)
+	timePassed := make(chan bool)
+	go sleeper(*t, timePassed)
+	go asker(input)
 
-	for _, record := range records {
-		fmt.Println("What is " + record[0] + "?")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimRight(input, "\n")
-		if input == record[1] {
-			correct++
+	var counter int
+	var timeout bool
+	var record []string
+	for counter < len(records) && !timeout {
+		record = records[counter]
+		fmt.Printf("What is %s?\n", record[0])
+		select {
+		case i := <-input:
+			if i == record[1] {
+				correct++
+			}
+		case <-timePassed:
+			fmt.Println("Timeout!")
+			timeout = true
 		}
+		counter++
 	}
+
 	fmt.Printf("Score: %d/%d\n", correct, totalNumberOfQuestions)
 }
